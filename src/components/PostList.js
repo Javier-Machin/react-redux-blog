129e396e907bc39ef105f8087b854087afaf9ff8
Javier Machin <javier_machin_acosta@hotmail.com>
2018-12-03 12:48:08 +0000

    Add postsReducer, add PostList renderList helper function

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,14 +8,34 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div classNAme="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  }
+
   render() {
     return (
-      <div>Post List</div>
+      <div className="ui relaxed divided list">{this.renderList()}</div>
     );
   };
 };
 
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostList);
