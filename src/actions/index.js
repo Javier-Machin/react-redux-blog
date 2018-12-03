23be5ec08063e02c893562b93f205a593cd07dc4
Javier Machin <javier_machin_acosta@hotmail.com>
2018-12-03 14:18:48 +0000

    Add fetchPostsAndUsers action (solution 2)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,47 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // Alternative using lodash chain
+
+  // _.chain(getState().posts)
+  //   .map('userId')
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value() // value tells lodash to execute the chain
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+
+// Lodash memoize version
+
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+// fetchUser with regular function syntax
 
 // export const fetchUser = function(id) {
 //   return async function(dispatch) {
